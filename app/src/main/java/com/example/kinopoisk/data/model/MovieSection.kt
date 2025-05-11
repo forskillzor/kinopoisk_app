fddb45cc35066e42df6be340a27c57c3789e75ad
Sergey Orlov <formyfrontend@gmail.com>
2025-05-11 16:08:04 +0700

    before refactoring Repository to interface

---
+++ app/src/main/java/com/example/kinopoisk/data/model/MovieSection.kt
@@ -0,0 +1,15 @@
+package com.example.kinopoisk.data.model
+
+data class MovieSection(
+    val title: String,
+    val movies: List<Movie>,
+    val type: SectionType
+)
+
+enum class SectionType {
+    PREMIERES,
+    POPULAR,
+    DYNAMIC_GENRE_COUNTRY,
+    TOP_250,
+    SERIES
+}
