73477c46d801c3fcdc4ffd00938c4347309d5e85
Sergey Orlov <skillzorwispa@gmail.com>
2025-05-12 14:37:54 +0700

    done HomepageFragment.kt

--- app/src/main/java/com/example/kinopoisk/presentation/homepage/HomeViewModel.kt
+++ app/src/main/java/com/example/kinopoisk/presentation/homepage/HomeViewModel.kt
@@ -4,26 +4,45 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.kinopoisk.data.Repository
+import com.example.kinopoisk.data.model.MovieSection
+import com.example.kinopoisk.data.model.SectionType
 import dagger.hilt.android.lifecycle.HiltViewModel
 import jakarta.inject.Inject
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
     private val repository: Repository
-): ViewModel() {
+) : ViewModel() {
     private val _uiState = MutableStateFlow<HomeUiState>(HomeUiState.Loading)
     val uiState: StateFlow<HomeUiState> = _uiState
 
     init {
         loadSections()
     }
+
     private fun loadSections() {
         viewModelScope.launch {
-            repository.getPremieres().collect { list ->
-                Log.d("HomeViewModel", "$list")
+            runCatching {
+                val premieres = repository.getPremieres().first()
+                val popular = repository.getPopular().first()
+                val top250 = repository.getTop250().first()
+                val series = repository.getSeries().first()
+                listOf(
+                    MovieSection("Премьеры", premieres, SectionType.PREMIERES),
+                    MovieSection("Популярноое", popular, SectionType.POPULAR),
+                    MovieSection("Топ-250", top250, SectionType.TOP_250),
+                    MovieSection("Сериалы", series, SectionType.SERIES),
+                )
+            }.onSuccess { sections ->
+                _uiState.value = HomeUiState.Success(sections)
+            }.onFailure { error ->
+                _uiState.value = HomeUiState.Error(error)
             }
         }
     }
--- app/src/main/java/com/example/kinopoisk/presentation/homepage/HomepageFragment.kt
+++ app/src/main/java/com/example/kinopoisk/presentation/homepage/HomepageFragment.kt
@@ -1,15 +1,24 @@
 package com.example.kinopoisk.presentation.homepage
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.kinopoisk.databinding.FragmentHomepageBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class HomepageFragment : Fragment() {
+    private val viewModel: HomeViewModel by viewModels()
+    private lateinit var homeAdapter: HomeAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -18,6 +27,32 @@ class HomepageFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val binding = FragmentHomepageBinding.inflate(inflater, container, false)
+
+        homeAdapter = HomeAdapter()
+        binding.sectionRecycler.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = homeAdapter
+        }
+
+
+        lifecycleScope.launch {
+            viewModel.uiState.collect { state ->
+                when(state) {
+                    is HomeUiState.Loading -> {
+                        binding.progressBar.visibility = View.VISIBLE
+                    }
+                    is HomeUiState.Success -> {
+                        binding.progressBar.visibility = View.GONE
+                        homeAdapter.submitList(state.sections)
+                    }
+                    is HomeUiState.Error -> {
+                        binding.progressBar.visibility = View.GONE
+                        Toast.makeText(context, "mesage", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
+
         return binding.root
     }
 }
