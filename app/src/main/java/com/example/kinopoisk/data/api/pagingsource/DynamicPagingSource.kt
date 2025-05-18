5f90726ffaf5340941726820ae0350202fef0f93
Sergey Orlov <skillzorwispa@gmail.com>
2025-05-18 07:04:04 +0700

    create paging sources for each section

---
+++ app/src/main/java/com/example/kinopoisk/data/api/pagingsource/DynamicPagingSource.kt
@@ -0,0 +1,30 @@
+package com.example.kinopoisk.data.api.pagingsource
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.example.kinopoisk.data.api.KinopoiskApi
+import com.example.kinopoisk.data.model.MovieDto
+import com.example.kinopoisk.domain.entities.Movie
+import com.example.kinopoisk.domain.mappers.toDomain
+
+class DynamicPagingSource(
+    private val api: KinopoiskApi
+): PagingSource<Int, Movie> () {
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Movie> {
+        val page = params.key?: 1
+        val response = api.top(page)
+        return LoadResult.Page(
+            data = response.films.map(MovieDto::toDomain),
+            prevKey = if(page == 1) null else page - 1,
+            nextKey = page + 1
+        )
+    }
+
+    override fun getRefreshKey(state: PagingState<Int, Movie>): Int? {
+        return state.anchorPosition?.let { anchor ->
+            state.closestPageToPosition(anchor)?.prevKey?.plus(1)
+                ?: state.closestPageToPosition(anchor)?.nextKey?.minus(1)
+        }
+    }
+}
